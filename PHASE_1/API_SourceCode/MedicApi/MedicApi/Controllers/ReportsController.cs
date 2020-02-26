6fc371e01cc42c770ac50dfa9759608049b699b5
Abanob Tawfik <abanob.tawfik@outlook.com>
2020-02-26 11:06:29 +1100

    testing automatic deployment on commit

--- PHASE_1/API_SourceCode/MedicApi/MedicApi/Controllers/ReportsController.cs
+++ PHASE_1/API_SourceCode/MedicApi/MedicApi/Controllers/ReportsController.cs
@@ -24,7 +24,7 @@ namespace MedicApi.Controllers
         [Route("Test2")]
         public ActionResult TestEndPoint2()
         {
-            return Ok("testing CI/CD pipeline working");
+            return Ok("testing CI/CD pipeline working automatically on push");
         }
     }
 }
