b1010860122a4ad0511b17a86c6ffe37c5dd2ec9
Abanob Tawfik <abanob.tawfik@outlook.com>
2020-03-06 00:32:04 +1100

    test mongodb client

---
+++ PHASE_1/API_SourceCode/MedicApi/MedicApi/Services/testdb.cs
@@ -0,0 +1,33 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MedicApi.Services
+{
+    public class testdb
+    {
+        public async void test()
+        {
+            var client = new MongoClient("mongodb+srv://medics:adfrZUBj4IF4TNibOnLxQKansolSPoW6@cluster0-nqmfu.mongodb.net/test?retryWrites=true&w=majority");
+            var db = client.GetDatabase("dev-reports");
+            var collections = db.GetCollection<BsonDocument>("reports");
+            var document = new BsonDocument
+            {
+                {"name", "MongoDB" },
+                {"type", "Database" },
+                {"count", 1 },
+                {"info", new BsonDocument
+                    {
+                        {"x", 203 },
+                        {"y", 102 },
+                    }
+                },
+            };
+            await collections.InsertOneAsync(document);
+            
+        }
+    }
+}
