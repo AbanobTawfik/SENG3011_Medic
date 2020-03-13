4baf772987fc95bf8fe04cd6e77e7014100d0b50
Kevin Luxa <k.luxa@unsw.edu.au>
2020-03-14 01:30:00 +1100

    Filtering reports

--- PHASE_1/API_SourceCode/MedicApi/MedicApi/Services/Generator.cs
+++ PHASE_1/API_SourceCode/MedicApi/MedicApi/Services/Generator.cs
@@ -11,7 +11,7 @@ namespace MedicApi.Services
 {
     public class Generator
     {
-        static readonly MongoClient client = new MongoClient("mongodb+srv://medics:adfrZUBj4IF4TNibOnLxQKansolSPoW6@cluster0-nqmfu.mongodb.net/test?retryWrites=true&ampampampampampampampampampampampampampw=majority");
+        static readonly MongoClient client = new MongoClient("mongodb+srv://medics:adfrZUBj4IF4TNibOnLxQKansolSPoW6@cluster0-nqmfu.mongodb.net/test?retryWrites=true&w=majority");
 
         public async void GenerateAdd()
         {
@@ -24,18 +24,27 @@ namespace MedicApi.Services
                 {
                     new StoredArticle
                     {
-                        url = "url11",
-                        date_of_publication_str = "2020-03-08 xx:xx:xx",
-                        headline = "Headline 11",
-                        main_text = "This is the main text of article 11.",
+                        url = "url12",
+                        date_of_publication_start = new DateTime(2019, 11, 27),
+                        date_of_publication_end = new DateTime(2019, 11, 27, 23, 59, 59),
+                        date_of_publication_str = "2019-11-27 xx:xx:xx",
+                        headline = "Headline 12",
+                        main_text = "This is the main text of article 12.",
+                        keywords = new List<string>
+                        {
+                            "coronavirus",
+                            "covid-19",
+                            "virus",
+                        },
                         reports = new List<StoredReport>
                         {
                             new StoredReport
                             {
-                                diseases = new List<string> { "2019 nCov" },
+                                diseases = new List<string> { "COVID-19" },
                                 syndromes = new List<string> { "Acute respiratory syndrome" },
-                                event_date_start = new DateTime(2020, 3, 5),
-                                event_date_end = new DateTime(2020, 3, 7),
+                                event_date_start = new DateTime(2019, 11, 25),
+                                event_date_end = new DateTime(2019, 11, 26, 23, 59, 59),
+                                event_date_str = "2019-11-25 xx:xx:xx to 2019-11-26 xx:xx:xx",
                                 locations = new List<StoredPlace>
                                 {
                                     new StoredPlace
@@ -53,7 +62,7 @@ namespace MedicApi.Services
             }
             catch (Exception e)
             {
-                Console.WriteLine(e);
+                Console.WriteLine("EXCEPTION:\n" + e);
             }
         }
     }
--- PHASE_1/API_SourceCode/MedicApi/MedicApi/Services/ReportFinder.cs
+++ PHASE_1/API_SourceCode/MedicApi/MedicApi/Services/ReportFinder.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 using MedicApi.Models;
 using MongoDB.Bson;
@@ -12,22 +13,39 @@ namespace MedicApi.Services
 {
     class ReportFinder
     {
-        static readonly MongoClient client = new MongoClient("mongodb+srv://medics:adfrZUBj4IF4TNibOnLxQKansolSPoW6@cluster0-nqmfu.mongodb.net/test?retryWrites=true&ampampampampampampampampampampampampampw=majority");
+        static readonly MongoClient client = new MongoClient("mongodb+srv://medics:adfrZUBj4IF4TNibOnLxQKansolSPoW6@cluster0-nqmfu.mongodb.net/test?retryWrites=true&w=majority");
 
         public List<Article> FindReports(string start_date,
-                                          string end_date,  string timezone,
-                                          string key_terms, string location,
-                                          int max, int offset)
+                                         string end_date,  string timezone,
+                                         string key_terms, string location,
+                                         int max, int offset)
         {
             DateTime start = DateTime.Parse(start_date); // don't use NodaTime for now
             DateTime end = DateTime.Parse(end_date);     // don't use NodaTime for now
             // ignore timezone for now, assume all timezones are AEST/AEDT
 
-            return RetrieveReports(start, end, key_terms, location, max, offset);
+            if (location == null)
+            {
+                location = "";
+            }
+
+            List<string> key_term_list;
+            if (key_terms == null)
+            {
+                key_term_list = new List<string> { };
+            }
+            else
+            {
+                key_term_list = key_terms.Split(',').ToList<string>()
+                                         .Select(t => t.Trim().ToLower())
+                                         .ToList();
+            }
+
+            return RetrieveReports(start, end, key_term_list, location.ToLower(), max, offset);
         }
 
         public List<Article> RetrieveReports(DateTime start, DateTime end,
-                                             string key_terms, string location,
+                                             List<string> key_terms, string location,
                                              int max, int offset)
         {
             try
@@ -36,7 +54,39 @@ namespace MedicApi.Services
 
                 var collections = db.GetCollection<StoredArticle>("reports");
 
-                var articles = collections.Find(x => x.url == "url1").ToList();
+                var articles = collections.Find(a =>
+                    // date match
+                    (
+                        a.date_of_publication_start <= end &&
+                        start <= a.date_of_publication_end
+                    )
+                    
+                    &&
+                    
+                    // location match
+                    (
+                        location == "" ||
+                        a.reports.Any(r =>
+                            r.locations.Any(l =>
+                                l.country.ToLower().Contains(location) ||
+                                l.location.ToLower().Contains(location)
+                            )
+                        )
+                    )
+
+                    &&
+
+                    // key term match
+                    (
+                        key_terms.Count == 0 ||
+                        key_terms.Any(t =>
+                            a.keywords.Contains(t)
+                        )
+                    )
+                )
+                .Skip(offset)
+                .Limit(max)
+                .ToList();
 
                 return articles.Select(a => a.ToArticle()).ToList();
             }
