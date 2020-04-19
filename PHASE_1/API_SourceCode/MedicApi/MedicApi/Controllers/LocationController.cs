e5541a85d821cc203b95641c56688f54045ae65d
Abanob Tawfik <goonpizza@hotmail.com>
2020-04-19 21:47:36 +1000

    added location db models

---
+++ PHASE_1/API_SourceCode/MedicApi/MedicApi/Controllers/LocationController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MedicApi.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MedicApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [ApiExplorerSettings(IgnoreApi = true)]
+    public class LocationController : ControllerBase
+    {
+        private Scraper _scraperService;
+
+        [HttpGet]
+        [Route("GeoId")]
+        // GET api/Location/GeoId
+        public ActionResult TestGenerate()
+        {
+            var service = new Generator();
+            service.GenerateAdd();
+            return Ok("done");
+        }
+
+        // GET api/Location/GeoName
+        [HttpGet]
+        [Route("GeoName")]
+        public ActionResult TestRSS()
+        {
+            _scraperService.ScrapeAndStoreOutbreaksFromRSS("https://tools.cdc.gov/api/v2/resources/media/285676.rss");
+            return Ok("done");
+        }
+    }
+}
---
+++ PHASE_1/API_SourceCode/MedicApi/MedicApi/Models/FrontEndLocation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MedicApi.Models
+{
+    public class FrontEndLocation
+    {
+        public string Country { get; set; }
+        public string Location { get; set; }
+        public string GeoId { get; set; }
+        public string Latitude { get; set; }
+        public string Longtitude { get; set; }
+    }
+}
