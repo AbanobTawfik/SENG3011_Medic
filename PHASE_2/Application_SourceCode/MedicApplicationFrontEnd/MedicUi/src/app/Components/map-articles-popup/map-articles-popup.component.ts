829d1daa623cd0937ac226421a56bf53567ba1c1
Abanob Tawfik <goonpizza@hotmail.com>
2020-04-23 19:46:12 +1000

    working on popup article

--- PHASE_2/Application_SourceCode/MedicApplicationFrontEnd/MedicUi/src/app/Components/map-articles-popup/map-articles-popup.component.ts
+++ PHASE_2/Application_SourceCode/MedicApplicationFrontEnd/MedicUi/src/app/Components/map-articles-popup/map-articles-popup.component.ts
@@ -5,6 +5,8 @@ import { NgbModalConfig, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import StandardArticle from "../../types/StandardArticle";
 
 import { MapArticleModalComponent } from '../map-article-modal/map-article-modal.component';
+import StandardReport from '../../types/StandardReport';
+import StandardLocation from '../../types/StandardLocation';
 
 @Component({
   selector: 'app-map-articles-popup',
@@ -14,7 +16,7 @@ import { MapArticleModalComponent } from '../map-article-modal/map-article-modal
 export class MapArticlesPopupComponent implements OnInit {
 
   @Input() articles: StandardArticle[];
-
+  loaded = false;
   constructor(config: NgbModalConfig, private modalService: NgbModal) {
     config.centered = true;
     config.size = "lg";
@@ -29,14 +31,16 @@ export class MapArticlesPopupComponent implements OnInit {
   }
 
   openArticleModal(article: StandardArticle) {
-    console.log(`Trying to open '${article.headline}'`);
+    alert(JSON.stringify(article));
     const modalRef = this.modalService.open(MapArticleModalComponent);
     modalRef.componentInstance.article = article;
   }
 
   getPopupArticles() {
-    const sorted = this.sortedArticles();
-    return sorted.slice(0, 10);
+    // console.log(this.articles.length);
+    // const sorted = this.sortedArticles();
+    //console.log(JSON.stringify(sorted));
+    return this.articles;
   }
 
   sortedArticles() {
